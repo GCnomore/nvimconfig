1cfa2341406c7c40f761c365652011358b288597
Isaac Choi <isaacgc08@gmail.com>
2024-07-21 23:31:45 -0700

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,5 +10,6 @@ map("i", "jk", "<ESC>")
 -- Custom
 map("v", "<", "<gv")
 map("v", ">", ">gv")
+map("n", "<leader>tw", "<cmd>TailwindSort<cr>", { desc = "Sort Tailwind classes" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
