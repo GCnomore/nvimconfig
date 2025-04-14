13ac3b3410d78f8d8bb50f3e711bb20893465b6f
Isaac Choi <isaacgc08@gmail.com>
2025-04-13 23:29:00 -0700

    Update mappings.lua

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,12 +4,24 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- Normal Mode
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>lf", function()
+   vim.diagnostic.open_float { border = "rounded" }
+end, {
+   desc = "Floating diagnostic",
+})
+
+map("n", "j", "gj")
+map("n", "k", "gk")
+
+-- Neogit Mapping
+map("n", "<leader>gg", ":Neogit<CR>", { desc = "Open Neogit" })
+
+-- Insert Mode
 map("i", "jk", "<ESC>")
+map("i", "ppp", "<C-R>+")
 
--- Custom
-map("v", "<", "<gv")
-map("v", ">", ">gv")
-map("n", "<leader>tw", "<cmd>TailwindSort<cr>", { desc = "Sort Tailwind classes" })
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Visual Mode
+map("v", "<", "<gv", { desc = "Indent line" })
+map("v", ">", ">gv", { desc = "Indent line" })
